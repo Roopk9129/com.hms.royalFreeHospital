ebcfedba35e7b704d027fcefa91fff4ad76a3403
Roopk9129 <roopk9129@gmail.com>
2023-09-13 17:46:33 +0530

    Last Commit

--- src/main/java/com/hms/ObjectRepo/AddDoctorSpecializationPage.java
+++ src/main/java/com/hms/ObjectRepo/AddDoctorSpecializationPage.java
@@ -10,12 +10,32 @@ public class AddDoctorSpecializationPage {
 	@FindBy(xpath = "//input[@name='doctorspecilization']")
 	private WebElement BetweenwdatesreportsLink;
 	
+	@FindBy(xpath="//button[@name='submit']")
+	private WebElement SubmitBtn;
 	
+	@FindBy(xpath="//select[@name='Doctorspecialization']")
+	private WebElement DoctorSpecializationDD;
 	
+	@FindBy(xpath="//input[@name='docname']")
+	private WebElement DoctorNameEdt;
 	
+	@FindBy(xpath="//textarea[@name='clinicaddress']")
+	private WebElement ClinicAddress;
 	
+	@FindBy(xpath="//input[@name='docfees']")
+	private WebElement DoctorConsultancyFeesEdt;
 	
+	@FindBy(xpath="//input[@name='doccontact']")
+	private WebElement DoctorContactNoEdt;
 	
+	@FindBy(xpath="//input[@name='docemail']")
+	private WebElement DoctorEmailEdt;
+	
+	@FindBy(xpath="//input[@name='npass']")
+	private WebElement DoctorPsdEdt;
+	
+	@FindBy(xpath="//input[@name='cfpass']")
+	private WebElement DoctorCfPsdEdt;
 	
 	public AddDoctorSpecializationPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
